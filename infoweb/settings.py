2af2696e3817eaf278b8ff90295cbe51fadd3cbc
soul3vans <elbert.escobar23@gmail.com>
2025-08-10 16:59:04 +0200

    Ojo al gato: 	modified:   .vscode/launch.json 	modified:   infoweb/settings.py 	new file:   media/archivos/normal.txt 	new file:   media/archivos/test.txt 	new file:   media/archivos/test_I2CpkTD.docx

--- infoweb/settings.py
+++ infoweb/settings.py
@@ -73,8 +73,8 @@ ROOT_URLCONF = 'infoweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'static/templates'],
-        'APP_DIRS': False,
+        'DIRS': [BASE_DIR / 'static/templates'],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
